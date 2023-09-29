6264dda6ec5501e7ffa418ea34639c338dad36c6
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-09-29 19:59:59 -0300

    configuracion de puerto para heroku

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-const PORT = process.env.PORT || 3000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
