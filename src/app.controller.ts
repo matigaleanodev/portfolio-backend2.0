9ceffed49f3f716b53f6634990bb3ca84718bc0a
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-09-29 22:55:19 -0300

    cambio de mensaje de inicio de api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getStart(): string {
+  getStart() {
     return this.appService.getStart();
   }
 }
