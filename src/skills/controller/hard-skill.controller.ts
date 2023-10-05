124985155e3512c6800d4409d43fc9281c134421
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-10-05 14:46:08 -0300

    metodos en controller de hard skill

--- src/skills/controller/hard-skill.controller.ts
+++ src/skills/controller/hard-skill.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { HardSkillService } from '../service/hard-skill.service';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { HardSkillEntity } from '../models/hard-skill.entity';
+import { UpdateHardSkillDTO } from '../dto/update-hard-skill.dto';
+import { Observable } from 'rxjs';
+import { CreateHardSkillDTO } from '../dto/create-hard-skill.dto';
 
 @Controller('hard-skill')
-export class HardSkillController {}
+export class HardSkillController {
+  constructor(private service: HardSkillService) {}
+
+  //Rest call: /api/hard-skill
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(@Body() skill: CreateHardSkillDTO): Observable<HardSkillEntity> {
+    return this.service.createSkill(skill);
+  }
+
+  //Rest call: /api/hard-skill
+  @Get()
+  getProjects(): Observable<HardSkillEntity[]> {
+    return this.service.getSkills();
+  }
+
+  //Rest call: /api/hard-skill/{id}
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  delete(
+    @Param('id', ParseIntPipe) id: number,
+  ): Observable<HardSkillEntity | HttpException> {
+    return this.service.deleteSkill(id);
+  }
+
+  //Rest call: /api/hard-skill/{id}
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() skill: UpdateHardSkillDTO,
+  ): Observable<HardSkillEntity | HttpException> {
+    return this.service.updateSkill(id, skill);
+  }
+
+  //Rest call: /api/hard-skill/{id}
+  @Get(':id')
+  getProject(
+    @Param('id', ParseIntPipe) id: number,
+  ): Observable<HardSkillEntity | HttpException> {
+    const result = this.service.getSkillById(id);
+    return result;
+  }
+}
