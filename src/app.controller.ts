04eb4e7c8807f87dca520956e814b95f7cf7aadc
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-10-23 12:34:36 -0300

    controller y service para sitemap

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('sitemap.xml')
+  @ApiExcludeEndpoint()
   async generateSitemap(@Res() res: Response) {
     const xmlContent = this.appService.generateSitemapXML();
 
