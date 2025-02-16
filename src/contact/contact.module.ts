d2b041cb5674b3fd94c6bd86699bbfbb040f065a
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-02-16 20:29:07 -0300

    feat(contact): agregar servicio para enviar mensajes de contacto por email

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -4,10 +4,11 @@ import { ContactService } from './service/contact.service';
 import { ContactEntity } from './models/contact.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { EmailService } from './service/email.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ContactEntity]), AuthModule],
   controllers: [ContactController],
-  providers: [ContactService],
+  providers: [ContactService, EmailService],
 })
 export class ContactModule {}
